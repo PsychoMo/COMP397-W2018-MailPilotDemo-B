44facfee5f8ffab8fbbdd084d42023ab971a5c33
PsychoMo <zymike09@hotmail.com>
2018-02-24 11:13:54 -0500

    added ocean to scene

--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -1,9 +1,9 @@
 module scenes {
     export class PlayScene extends objects.Scene {
         // Private Instance Variables
-        
+        private _ocean: objects.Ocean;
 
-        
+
         // Public Properties
 
         // Constructor
@@ -20,17 +20,18 @@ module scenes {
 
         // Initialize Game Variables and Objects
         public Start(): void {
-           
+            this._ocean = new objects.Ocean(this.assetManager);
             this.Main();
         }
 
         public Update(): void {
-            
+            this._ocean.Update();
         }
 
         // This is where the fun happens
         public Main(): void {
-            
+            // add the ocean to the scene
+            this.addChild(this._ocean);
         }
     }
 }
